b476a16c07f91905cf64e745c63e641d9f0246c3
Vicki Bills <P15182642@LEC-ADMIN.dmu.ac.uk>
2017-03-15 12:54:41 +0000

    Updated the Product classes and tests

---
+++ MyClassLib/clsProductCollection.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductClassLibrary
+{
+    public class clsProductCollection
+    {
+        //private data member for the list
+        List<clsProduct> mProductList = new List<clsProduct>();
+
+        //constructor for the class
+        public clsProductCollection()
+        {
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount = 0;
+            //object for the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblProduct_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank product
+                clsProduct AProduct = new clsProduct();
+                //read in the fields from the current record
+                AProduct.CategoryID = Convert.ToInt32(DB.DataTable.Rows[Index]["Category_ID"]);
+                AProduct.NoAvailable = Convert.ToInt32(DB.DataTable.Rows[Index]["No_Available"]);
+                AProduct.ProductDetail = Convert.ToString(DB.DataTable.Rows[Index]["Product_Detail"]);
+                AProduct.ProductID = Convert.ToInt32(DB.DataTable.Rows[Index]["Product_ID"]);
+                AProduct.ProductName = Convert.ToString(DB.DataTable.Rows[Index]["Product_Name"]);
+                AProduct.ProductPrice = Convert.ToDecimal(DB.DataTable.Rows[Index]["Product_Price"]);
+                //add the record to the private data member
+                mProductList.Add(AProduct);
+                //point at the next record
+                Index++;
+            }
+        }
+
+        public int Count //public property for count
+        {
+            get
+            {
+                //return the count of the list
+                return mProductList.Count;
+            }
+            set
+            {
+                //we shall worry about this later
+            }
+        }
+
+        public List<clsProduct> ProductList //public property for the product list 
+        {
+            get
+            {
+                //return the private data
+                return mProductList;
+            }
+            set
+            {
+                //set the private data 
+                mProductList = value;
+            }
+        }
+        public clsProduct ThisProduct { get; set; }
+    }
+}
---
+++ MyTestUnit/tstProductCollection.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductClassLibrary;
+using System.Collections.Generic;
+
+namespace Test_Framework
+{
+    /// <summary>
+    /// Summary description for tstProductCollection
+    /// </summary>
+    [TestClass]
+    public class tstProductCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsProductCollection AllProducts = new clsProductCollection();
+            //test to see that it exists
+            Assert.IsNotNull(AllProducts);
+        }
+
+        [TestMethod]
+        public void ProductListOK()
+        {
+            //create an instance for the class we want to create
+            clsProductCollection AllProducts = new clsProductCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsProduct> TestList = new List<clsProduct>();
+            //add an item to the list
+            //create the item of the test data
+            clsProduct TestItem = new clsProduct();
+            //set its properties
+            TestItem.CategoryID = 2;
+            TestItem.NoAvailable = 10;
+            TestItem.ProductDetail = "used";
+            TestItem.ProductID = 3;
+            TestItem.ProductName = "laptop";
+            TestItem.ProductPrice = 650.00m;
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllProducts.ProductList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllProducts.ProductList, TestList);
+        }
+
+        
+
+        [TestMethod]
+        public void ThisProductPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsProductCollection AllProducts = new clsProductCollection();
+            //create some test data to assign to the property
+            clsProduct TestProduct = new clsProduct();
+            //setthe properties of the test object
+            TestProduct.CategoryID = 2;
+            TestProduct.NoAvailable = 10;
+            TestProduct.ProductDetail = "used";
+            TestProduct.ProductID = 3;
+            TestProduct.ProductName = "laptop";
+            TestProduct.ProductPrice = 650.00m;
+            //assign the data to the property
+            AllProducts.ThisProduct = TestProduct;
+            //test to see the two values are the same
+            Assert.AreEqual(AllProducts.ThisProduct, TestProduct);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class we want to create
+            clsProductCollection AllProducts = new clsProductCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsProduct> TestList = new List<clsProduct>();
+            //add new item to list
+            //create the item of test data
+            clsProduct TestItem = new clsProduct();
+            TestItem.CategoryID = 2;
+            TestItem.NoAvailable = 10;
+            TestItem.ProductDetail = "used";
+            TestItem.ProductID = 3;
+            TestItem.ProductName = "laptop";
+            TestItem.ProductPrice = 650.00m;
+            //add the item to the list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllProducts.ProductList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllProducts.Count, TestList.Count);
+        }
+
+    }
+
+
+}
